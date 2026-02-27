c8f082c9a6372ccd437b229b43b0570b44a46948
meontd369-alt <meontd369@gmail.com>
2026-02-27 19:14:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if st.button("🔍 Bắt Đầu Quét & Phân Tích Rủi Ro"):
     else:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-pro-latest')
+            model = genai.GenerativeModel('gemini-1.5-pro')
 
             with st.spinner("⏳ Chuyên gia AI đang đối chiếu dữ liệu với Luật Thuế. Vui lòng đợi..."):
                 
@@ -104,4 +104,5 @@ if st.button("🔍 Bắt Đầu Quét & Phân Tích Rủi Ro"):
             st.write(response.text)
 
         except Exception as e:
+
             st.error(f"Đã xảy ra lỗi hệ thống: {e}")
